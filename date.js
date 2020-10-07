8d935c604d1175ba24b89076ac9ed6d7812abd17
Jjin SoL <parkleydondon@yahoo.com>
2020-10-07 00:35:39 -0700

    Initial commit

---
+++ date.js
@@ -0,0 +1,12 @@
+
+exports.getDate = function () {
+  const today = new Date();
+  const options = { weekday: "long", year: "numeric", month: "long", day: "numeric"};
+  return today.toLocaleDateString("en-US", options);
+};
+
+exports.getDay = function getDay() {
+  const today = new Date();
+  const options = { weekday: "long"};
+  return today.toLocaleDateString("en-US", options);
+};
