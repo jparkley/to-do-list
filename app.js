a72cdf54bbaddf13b7fb520708bffe44d12bcb72
Jjin SoL <parkleydondon@yahoo.com>
2020-10-07 13:05:29 -0700

    Change port to support Heroku

--- app.js
+++ app.js
@@ -44,6 +44,10 @@ app.get("/about", function(req, res) {
   res.render("about");
 });
 
-app.listen(3000, function() {
-  console.log("server started on port 3000");
+// app.listen(3000, function() {
+//   console.log("server started on port 3000");
+// });
+
+app.listen(process.env.PORT || 3000, function() {
+  console.log("server started");
 });
